993325b7efcdb826b8fe111cd56e4d7d160a2b0a
teddy <teddy@protopie.io>
2021-12-09 14:25:00 +0900

    [fix] fixed incorrect b and g value  on Demo Input

--- Demo/index.ts
+++ Demo/index.ts
@@ -3,8 +3,8 @@ import {createColorPicker, RGB} from '../src/ColorPicker';
 
 const appEl = document.getElementById('app')!;
 const rEl = document.getElementById('rgb_r') as HTMLInputElement;
-const gEl = document.getElementById('rgb_b') as HTMLInputElement;
-const bEl = document.getElementById('rgb_g') as HTMLInputElement;
+const gEl = document.getElementById('rgb_g') as HTMLInputElement;
+const bEl = document.getElementById('rgb_b') as HTMLInputElement;
 const picker = createColorPicker({paletteWidth: 500, paletteHeight: 500, knobRadius: 10, gap: 10, sliderHeight: 20});
 picker.attach(appEl);
 picker.onColorChanged( (rgb: RGB) => {
